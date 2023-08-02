b7bb908735fefd432fe01471fc8fdfe44a5dc1c8
b0 <bewuket1621@gmail.com>
2023-08-02 08:25:59 -0400

    product page styled

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,33 @@
+import PageNav from "../components/PageNav";
+
 const Product = () => {
   return (
-    <div>
-      <h2>Product</h2>
-    </div>
+    <main className="h-screen bg-slate-600">
+      <PageNav />
+      <section className=" w-[800px] mx-auto text-white flex flex-row justify-between my-14">
+        <div className="h-80">
+          <img
+            className="  h-full"
+            src="img-1.jpg"
+            alt="person with dog overlooking mountain with sunset"
+          />
+        </div>
+        <div className=" w-[420px] my-5">
+          <h2 className="text-4xl"> About WorldWide.</h2>
+          <p className="my-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo est
+            dicta illum vero culpa cum quaerat architecto sapiente eius non
+            soluta, molestiae nihil laborum, placeat debitis, laboriosam at fuga
+            perspiciatis?
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+            doloribus libero sunt expedita ratione iusto, magni, id sapiente
+            sequi officiis et.
+          </p>
+        </div>
+      </section>
+    </main>
   );
 };
 
