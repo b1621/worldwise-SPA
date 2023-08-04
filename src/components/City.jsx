253dce9f0a7b4c9113f80c6bb53849512e1989ab
b0 <bewuket1621@gmail.com>
2023-08-04 12:32:46 -0400

    working on displaying city detail

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,6 @@
 import { useParams, useSearchParams } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
+import { useEffect } from "react";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -9,24 +11,45 @@ const formatDate = (date) =>
 
 const City = () => {
   const { id } = useParams();
+  const { getCity, currentCity } = useCities();
+
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+  useEffect(
+    function () {
+      getCity(id);
+    },
+    [id]
+  );
 
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
   const { cityName, emoji, date, notes } = currentCity;
 
   return (
     <div>
-      <p>city id {id}</p>
-      <h2>
-        Position:{lat}:{lng}
-      </h2>
+      <div>
+        <h6>City name</h6>
+        <h3>
+          <span> {emoji} </span> {cityName}
+        </h3>
+      </div>
+      {notes && (
+        <div className=" border my-2">
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
+      <div>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+          className=" text-blue-500"
+        >
+          Check out {cityName} on wikipedia &rarr;{" "}
+        </a>
+      </div>
     </div>
   );
 };
